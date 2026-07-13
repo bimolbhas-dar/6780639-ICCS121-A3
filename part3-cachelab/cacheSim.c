a47ec4101a754897b8c49930ffe9e92e027aeb89
bimolbhas-dar <bimolbhas.dar@student.mahidol.edu>
2026-07-13 15:55:27 +0000

    p3 done

--- part3-cachelab/cacheSim.c
+++ part3-cachelab/cacheSim.c
@@ -44,8 +44,9 @@ void printCache()
 // ------------P2-------------
 // Note: we will assume that every request is for a 4-byte data and address can start at any byte; use FIFO lowest timeStamp
 static u_int32_t fifoTime = 1;
-
 // helper functions
+
+//return the matching way number if the block is in cache (hit) timeStamp(valid)!=0 and tag match
 static int l1_hit(unsigned int l1Set, unsigned int l1Tag) {
 	for (int way = 0; way < 2; way++) {
         if (L1_cache[l1Set][way].timeStamp != 0 && L1_cache[l1Set][way].tag == l1Tag) {
@@ -62,17 +63,17 @@ static int l2_hit(unsigned int l2Set, unsigned int l2Tag) {
     }
     return -1;
 }
-	// found empty space or if not found use FIFO
+// miss l1 use fifo for replacement
 static int l1_fifo(unsigned int l1Set, unsigned int l1Tag) {
     int kickout = -1;
-    // find empty space
+    // search for valid=0 or empty
     for (int way = 0; way < 2; way++) {
         if (L1_cache[l1Set][way].timeStamp == 0) {
-            kickout = way;
+            kickout = way; 
             break;
         }
     }
-    // fifo: find 1st in
+    // no empty space -> use fifo: find oldest (lowest timestamp)
     if (kickout == -1) {
 		kickout=0;
         if (L1_cache[l1Set][1].timeStamp < L1_cache[l1Set][0].timeStamp) {
@@ -84,19 +85,19 @@ static int l1_fifo(unsigned int l1Set, unsigned int l1Tag) {
     L1_cache[l1Set][kickout].timeStamp = fifoTime++;
     return kickout;
 }
-// l2 miss as well -> load data from DRAM
+// l2 miss as well-> use fifo for replacement -> load data from DRAM
 static int l2_miss(unsigned int l2Set, unsigned int l2Tag, u_int32_t currentAddress) {
     int kickout = -1;
-    u_int32_t int blockStart = currentAddress & ~0xF;
+    u_int32_t blockStart = currentAddress & ~0xF;
     
-    // find empty space
+    // find empty space or valid=0
     for (int way = 0; way < 4; way++) {
         if (L2_cache[l2Set][way].timeStamp == 0) {
             kickout = way;
             break;
         }
     }
-    // set is full
+    // no empty space -> use fifo: find the oldest (lowest timestamp)
     if (kickout == -1) {
         kickout = 0;
 		// use fifo
@@ -105,28 +106,25 @@ static int l2_miss(unsigned int l2Set, unsigned int l2Tag, u_int32_t currentAddr
                 kickout = way; 
             }
         }
-		// if remove a block in l2, this block in l1 is removed as well
-    	u_int32_t oldBlockAddress =(L2_cache[l2Set][kickout].tag << 6) |(l2Set << 4);
-    	unsigned int oldL1Set = getL1SetID(oldBlockAddress);
-    	unsigned int oldL1Tag = getL1Tag(oldBlockAddress);
+		//finding the kickout's address
+    	u_int32_t oldAddress =(L2_cache[l2Set][kickout].tag << 6) |(l2Set << 4); 
+    	unsigned int oldL1Set = getL1SetID(oldAddress);
+    	unsigned int oldL1Tag = getL1Tag(oldAddress);
 
+		// find the address in l1 to make it invalid (timestamp=0)
     	for (int way = 0; way < 2; way++){
-        	if (L1_cache[oldL1Set][way].timeStamp != 0 &&
-            	L1_cache[oldL1Set][way].tag == oldL1Tag){
-            	L1_cache[oldL1Set][way].timeStamp = 0;}
+        	if (L1_cache[oldL1Set][way].timeStamp != 0 && L1_cache[oldL1Set][way].tag == oldL1Tag){
+            	L1_cache[oldL1Set][way].timeStamp = 0;
+			}
     	}
 	}
-
     // Load the block from DRAM into L2.
     L2_cache[l2Set][kickout].tag = l2Tag;
     L2_cache[l2Set][kickout].timeStamp = fifoTime++;
 
-    for (int i = 0; i < 16; i++)
-    {
-        L2_cache[l2Set][kickout].data[i] =
-            DRAM[blockStart + i];
-    }
-
+    for (int i = 0; i < 16; i++){
+        L2_cache[l2Set][kickout].data[i] =DRAM[blockStart + i];
+	}
     return kickout;
 }
 
@@ -134,13 +132,13 @@ u_int32_t read_fifo(u_int32_t address)
 {
     u_int32_t result = 0;
     for (int byteNumber = 0; byteNumber < 4; byteNumber++) {
-        u_int32_t currentAddress = address + byteNumber;
+        u_int32_t curAddress = address + byteNumber;
 
-        unsigned int l1Set = getL1SetID(currentAddress);
-        unsigned int l1Tag = getL1Tag(currentAddress);
-        unsigned int l2Set = getL2SetID(currentAddress);
-        unsigned int l2Tag = getL2Tag(currentAddress);
-        unsigned int offset = currentAddress & 0xF;
+        unsigned int l1Set = getL1SetID(curAddress);
+        unsigned int l1Tag = getL1Tag(curAddress);
+        unsigned int l2Set = getL2SetID(curAddress);
+        unsigned int l2Tag = getL2Tag(curAddress);
+        unsigned int offset = curAddress & 0xF;
 
         // find the block in l1
         int l1Way =l1_hit(l1Set, l1Tag);
@@ -151,10 +149,10 @@ u_int32_t read_fifo(u_int32_t address)
             
             //l2 miss
             if (l2Way == -1) {
-                l2Way =l2_miss(l2Set, l2Tag, currentAddress);
+                l2Way =l2_miss(l2Set, l2Tag, curAddress);
             }
             
-            // move the block from l2 intol1
+            // move the block from l2 into l1
             l1Way = l1_fifo(l1Set, l1Tag);
             for (int i = 0; i < 16; i++) {
                 L1_cache[l1Set][l1Way].data[i] = L2_cache[l2Set][l2Way].data[i];
@@ -162,7 +160,6 @@ u_int32_t read_fifo(u_int32_t address)
         }
         result |= ((u_int32_t)L1_cache[l1Set][l1Way].data[offset]) << (8 * byteNumber);
     }
-
     return result;
 }
 
@@ -198,9 +195,9 @@ unsigned int getL1SetID(u_int32_t address)
 {
 	u_int32_t movedright;
 	unsigned compare;
-	movedright=address>>4; //move 4 most right bits to replace offset bits; make set digit to be the most right
-	// #set digit =log2(2sets)=1 digit
-	compare=movedright & 0x1; // 0x1=0001; comparing the most right digit to see whether it's set 0 or 1; 1&1=1 1&0 =0
+	movedright=address>>4; //move 4 most right bits to replace offset bits; make set bits to be the most right
+	// #set bit =log2(2sets)=1 bit
+	compare=movedright & 0x1; // 0x1=0001; comparing the most right bit to see whether it's set 0 or 1; 1&1=1 1&0 =0
 	return compare;
 }
 
@@ -209,28 +206,54 @@ unsigned int getL2SetID(u_int32_t address)
 u_int32_t movedright;
 	unsigned compare;
 	movedright=address>>4;
-	// #set digit =log2(4sets)=2 digits 
+	// #set bits =log2(4sets)=2 bits 
 	compare=movedright & 0x3; // 0x3 =0011
 	return compare;
 }
 
 unsigned int getL1Tag(u_int32_t address)
 {
-return address>>5; // L1 tag bit starts at 5th digit in the address (offset 0-3, set 4, tag 5-31)
+return address>>5; // L1 tag bit starts at 5th bit in the address (offset 0-3, set 4, tag 5-31)
 }
 
 unsigned int getL2Tag(u_int32_t address)
 {
-return address >>6; // L2 tag bit start at 6th digit (offset 0-3, set 4-5, tag 6-31)
+return address >>6; // L2 tag bit start at 6th bit (offset 0-3, set 4-5, tag 6-31)
 }
 
 // --------P3---------
 void write(u_int32_t address, u_int32_t data)
 {
-///// REPLACE THIS /////
-	read_fifo(address);
-return;
+	read_fifo(address); // make sure that the data is actully in l1
+	for(int byteNumber=0; byteNumber<4;byteNumber++){
+		u_int32_t curAddress=address+byteNumber; 
+
+		unsigned int l1Set = getL1SetID(curAddress);
+    	unsigned int l1Tag = getL1Tag(curAddress);
+    	unsigned int l2Set = getL2SetID(curAddress);
+    	unsigned int l2Tag = getL2Tag(curAddress);
+    	unsigned int offset = curAddress & 0xF;
+
+		unsigned char byteVal = (data >> (8 * byteNumber)) & 0xFF;
+
+		// which way it's located
+		int l1Way = l1_hit(l1Set, l1Tag); 
+    	int l2Way = l2_hit(l2Set, l2Tag);
+
+		// update matching byte in l1
+    	if (l1Way != -1){
+        	L1_cache[l1Set][l1Way].data[offset] = byteVal;
+    	}
+		//update matching byte in l2
+    	if (l2Way != -1){
+        L2_cache[l2Set][l2Way].data[offset] = byteVal;
+    	}
+		// update DRAM
+    	DRAM[curAddress] = byteVal;
+	}
+	return;
 }
+
 
 
 int main()
